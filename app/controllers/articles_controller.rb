536c067ddd73602a74def40cdd645daea1fea946
rails udemy course <railsudemycourse@gmail.com>
2022-08-04 11:37:08 +0530

    add user validations

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,4 @@
+class ArticlesController <Applicationcontroller
+def new
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+
+
+  resources :articles
 end
 
 
